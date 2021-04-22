95547c30dd6312e9e0a88c6838e17d4b87d706fd
hyokyun <gyrbs22@gmail.com>
2021-04-23 00:11:17 +0900

    Update 02_02.py

--- week02/02_02.py
+++ week02/02_02.py
@@ -1,5 +1,3 @@
-
-
 # 첫 번째 줄에 정수 K가 주어진다. (1 ≤ K ≤ 100,000)
 # 이후 K개의 줄에 정수가 1개씩 주어진다. 정수는 0에서 1,000,000 사이의 값을 가지며, 정수가 "0" 일 경우에는 가장 최근에 쓴 수를 지우고, 아닐 경우 해당 수를 쓴다.
 # 정수가 "0"일 경우에 지울 수 있는 수가 있음을 보장할 수 있다.
@@ -11,7 +9,7 @@
 
 
 #조건입력
-Size_Array = int(input('RepeatNumber? '))
+Size_Array = int(input('Size? '))
 print(Size_Array)
 
 i = 0
@@ -38,7 +36,5 @@ while i < Size_Array:
 print(InputArray)
 
 #최종결과 도출
-sum = 0
-for k in range(InputArray):
-    sum = sum + int(InputArray[k])
-print(sum)
+result = sum(InputArray)
+print(result)
