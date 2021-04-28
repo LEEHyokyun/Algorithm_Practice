fab338aec77dc464aa94e2951d7412451e37f672
hyokyun <gyrbs22@gmail.com>
2021-04-29 00:43:53 +0900

    Update 03_01_DFS.py

--- week03/03_01_DFS.py
+++ week03/03_01_DFS.py
@@ -1,34 +1,43 @@
-#DFS
-#Depth first search
-#깊이우선탐색, 루트노드를 시작으로 깊이우선탐색하여 노드들을 조회하는 방법
+# DFS
+# Depth first search
+# 깊이우선탐색, 루트노드를 시작으로 깊이우선탐색하여 노드들을 조회하는 방법
 
-#Input : 정점의 개수 - 간선의 개수 - 시작노드
-# 1
-# 2 3 4
-# 4 4
+# Input : 정점의 개수 - 간선의 개수 - 시작노드
+# 1 2
+# 1 3
+# 1 4
+# 2 4
+# 3 4
 
 # 깊이탐색은 깊이우선, 1 2 4 3
 
+# 숫자를 입력받았을때 이를 그래프로 구현하는 로직
+
+# 먼저 그래프부터 만들기
 graph = {
-    1: [2, 3, 4],
-    2: [4],
-    3: [4]
+    1: [4, 2, 3],
+    2: [4, 1],
+    3: [4, 1],
+    4: [1, 2, 3]
 }
 
-def DFS_By_Stack(Input_graph, root_node):
+#DFS
+def dfs_by_stack(Input_graph, root_node):
 
     stack = [root_node]
-    visited = []
+    visited_node = []
 
-    #last in first out
+    # last in first out
     while stack:
         current_node = stack.pop()
-        visited.append(current_node)
+        visited_node.append(current_node)
 
         for near_node in Input_graph[current_node]:
-            if near_node not in visited :
+            if near_node not in visited_node:
                 stack.append(near_node)
 
-    return visited
 
-print(DFS_By_Stack(graph, 1))
+    return visited_node
+
+
+print(dfs_by_stack(graph, 1))
