350daf03eb425bf575b71ead70a13c32c2619a20
hyokyun <gyrbs22@gmail.com>
2021-04-29 23:59:35 +0900

    Update 03_01_DFS.py

--- week03/03_01_DFS.py
+++ week03/03_01_DFS.py
@@ -15,29 +15,33 @@
 
 # 먼저 그래프부터 만들기
 graph = {
-    1: [4, 2, 3],
-    2: [4, 1],
-    3: [4, 1],
-    4: [1, 2, 3]
+    1: [2, 4, 3],
+    2: [1, 4],
+    3: [1, 4],
+    4: [1, 3, 2]
 }
 
-#DFS
-def dfs_by_stack(Input_graph, root_node):
 
+# DFS
+# 완전이진트리가 아닌 정점이나 인접노드가 여러개인 그래프 등
+# 모두 적용가능한 코드필요
+# stack 이용
+
+def dfs(graph, root_node):
     stack = [root_node]
-    visited_node = []
+    result = []
 
-    # last in first out
     while stack:
-        current_node = stack.pop()
-        visited_node.append(current_node)
 
-        for near_node in Input_graph[current_node]:
-            if near_node not in visited_node:
-                stack.append(near_node)
+        parent_node = stack.pop()
+        children = graph[parent_node]
+        children_node = children.map()
+
+        if children_node not in result:
+            result.append(parent_node)
+            stack.extend(graph[parent_node])
+
+    return result
 
 
-    return visited_node
-
-
-print(dfs_by_stack(graph, 1))
+print(dfs(graph, 1))
